50a2f826a33c0506490727e5d856c3e8754af2b9
Benjamin Wong <tszkan.wong@gmail.com>
2020-12-26 17:31:41 +0800

    Support External TokenServer

--- src/main/kotlin/ok/work/etoroapi/client/AuthorizationContext.kt
+++ src/main/kotlin/ok/work/etoroapi/client/AuthorizationContext.kt
@@ -37,12 +37,29 @@ class AuthorizationContext {
         if (token != null) {
             exchangeToken = token
         } else {
-            auth(System.getenv("LOGIN"), System.getenv("PASSWORD"))
-            exchange()
+            val tokenServer = System.getenv("TOKEN_SERVER")
+            if (tokenServer != null) {
+                auth_by_token_server(tokenServer, System.getenv("LOGIN"), System.getenv("PASSWORD"))
+            } else {
+                auth(System.getenv("LOGIN"), System.getenv("PASSWORD"))
+                exchange()
+            }
         }
         getAccountData(TradingMode.REAL)
     }
-
+    private fun auth_by_token_server(tokenServer: String, username: String, pwd: String) {
+        val req = HttpRequest.newBuilder().uri(URI("http://${tokenServer}/auth"))
+                .header("content-type", "application/json;charset=UTF-8")
+                .header("accept", "application/json, text/plain, */*")
+                .POST(HttpRequest.BodyPublishers.ofString("{\"password\":\"${pwd}\",\"username\":\"${username}\"}"))
+                .build()
+        println(req)
+        val response = client.send(req, HttpResponse.BodyHandlers.ofString()).body()
+        println("==========================")
+        println(response)
+        println("==========================")
+        exchangeToken = JSONObject(response).getString("token").toString()
+    }
     private fun auth(username: String, pwd: String) {
         val req = HttpRequest.newBuilder().uri(URI("${metadataService.baseUrl}/api/sts/v2/oauth/auth?client_request_id=${requestId}"))
                 .header("authority", metadataService.domain)
@@ -51,8 +68,10 @@ class AuthorizationContext {
                 .header("content-type", "application/json;charset=UTF-8")
                 .header("accept", "application/json, text/plain, */*")
                 .header("x-sts-gatewayappid", "90631448-9A01-4860-9FA5-B4EBCDE5EA1D")
+//                .header("x-sts-clienttime", "2020-12-24T09:19:14")
+                .header("x-device-id", "699914f6-e983-4c26-bb86-41eaaf14de09")
                 .header("applicationidentifier", "ReToro")
-                .header("applicationversion", "212.0.7")
+                .header("applicationversion", "288.0.1")
                 .header("sec-fetch-site", "same-origin")
                 .header("sec-fetch-mode", "cors")
                 .header("referer", "${metadataService.baseUrl}/login")
@@ -60,8 +79,11 @@ class AuthorizationContext {
                // .header("cookie", "etoroHPRedirect=1; _ga=GA1.2.1096383890.1543357062; visid_incap_172517=ZNWYjpoOTt6IOfHx3HN5VxPG/VsAAAAAQUIPAAAAAACPZ88tOSyD62NooqBbZ/hN; visid_incap_773285=pfQdz3B9TZuve/PhmXgPnF3G/VsAAAAAQUIPAAAAAAC7uOLsspgsQRNnV9pm9LiA; fbm_166209726726710=base_domain=.etoro.com; liveagent_oref=; liveagent_ptid=b60ed7d3-3047-4752-86d1-14ba900fd3c4; _DCMN_id.90.13db=2a8b42dd8726053f.1543357972.4.1543872510.1543768287.; TMIS2=${credentialsService.getCredentials().tmis2}; _gat=1;")
                 .POST(HttpRequest.BodyPublishers.ofString("{\"Password\":\"${pwd}\",\"UserLoginIdentifier\":\"${username}\",\"Username\":\"${username}\",\"rememberMe\":true,\"RequestedScopes\":[]}"))
                 .build()
+        println(req)
         val response = client.send(req, HttpResponse.BodyHandlers.ofString()).body()
+        println("==========================")
         println(response)
+        println("==========================")
         accessToken = JSONObject(response).getString("accessToken").toString()
     }
 
@@ -82,6 +104,7 @@ class AuthorizationContext {
         val response = JSONObject(client.send(req, HttpResponse.BodyHandlers.ofString()).body())
                 .getJSONObject("AggregatedResult")
                 .getJSONObject("ApiResponses")
+        println(response)
         userdata = response.getJSONObject("CurrentUserData").getJSONObject("Content").getJSONArray("users").getJSONObject(0)
         realgcid = userdata.getInt("realCID").toString()
         demogcid = userdata.getInt("demoCID").toString()
