d18cdcde5ab6d484551a4acc611516ef0781afab
Morten Scheel <morten@mortenscheel.com>
2021-02-13 21:49:25 +0100

    Add username attribute to Mirror

--- src/main/kotlin/ok/work/etoroapi/model/Mirror.kt
+++ src/main/kotlin/ok/work/etoroapi/model/Mirror.kt
@@ -2,4 +2,4 @@ package ok.work.etoroapi.model
 
 data class Avatar(val width: Int, val height: Int, val type: String, val url: String)
 
-data class Mirror(val realCID: Int, val lastName: String, val firstName: String, val aboutMe: String, val avatars: List<Avatar>)
+data class Mirror(val realCID: Int, val username: String, val lastName: String, val firstName: String, val aboutMe: String, val avatars: List<Avatar>)
