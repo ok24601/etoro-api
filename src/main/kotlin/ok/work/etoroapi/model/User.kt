bd8c48afac6b8c43997bfced7aaa12c74a2163e2
Morten Scheel <morten@mortenscheel.com>
2021-02-14 19:31:53 +0100

    Ignore unknown properties in User

--- src/main/kotlin/ok/work/etoroapi/model/User.kt
+++ src/main/kotlin/ok/work/etoroapi/model/User.kt
@@ -1,7 +1,11 @@
 package ok.work.etoroapi.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Avatar(val width: Int, val height: Int, val type: String, val url: String)
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class User(
     val realCID: Int,
     val username: String,
