acb8bec8208e0c4b6dc0106f1105397b61838fca
Pedro Estima <p.estima@sensysgatso.com>
2022-06-14 14:45:17 +0200

    Add lightstreamer:3.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,9 @@ dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     compile  ("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 
-    compile("com.lightstreamer:ls-javase-client:3.1.1")
+    implementation(files("$projectDir/libs/ls-javase-client-3.1.1.jar"))
+    implementation("com.lightstreamer:ls-log-adapter-java:1.0.2")
+    implementation("com.lightstreamer:ls-javase-client:3.1.1")
 
     compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.2")
 
